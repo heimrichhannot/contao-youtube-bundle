5c9fae3fca893f44425c3f8d39311f34ddc9bc97
Thomas Körner <t.koerner@heimrich-hannot.de>
2018-07-06 09:12:47 +0200

    fixed video template

--- src/ContentElement/ContentYouTube.php
+++ src/ContentElement/ContentYouTube.php
@@ -7,11 +7,14 @@
 
 namespace HeimrichHannot\YoutubeBundle\ContentElement;
 
+use Contao\BackendTemplate;
 use Contao\ContentElement;
 use Contao\System;
 
 class ContentYouTube extends ContentElement
 {
+    const TYPE = 'youtube';
+
     protected $strTemplate = 'ce_youtube';
 
     /**
@@ -20,7 +23,7 @@ class ContentYouTube extends ContentElement
     public function generate()
     {
         if (System::getContainer()->get('huh.utils.container')->isBackend()) {
-            $this->Template = new \BackendTemplate('be_wildcard');
+            $this->Template = new BackendTemplate('be_wildcard');
             $this->Template->title = 'YouTube-Video '.$this->youtube;
 
             return $this->Template->parse();
